de1400285fddb1cf3e2a8fca5ce0990152bb3c30
juan manuel <jmcontrerasvega@gmail.com>
2024-06-08 09:58:46 -0500

    linea_base

---
+++ src/main/java/com/app/procesamiento_datos/ProcesamientoDatosApplication.java
@@ -0,0 +1,13 @@
+package com.app.procesamiento_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesamientoDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcesamientoDatosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/procesamiento_datos/ProcesamientoDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.procesamiento_datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesamientoDatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
